32e72ab855ba7bff318cfb859de6d4e151d82903
andre.borba <dev_andre.filipe@outlook.com>
2023-03-29 21:13:47 -0300

    feat: String module

---
+++ Modules/String.exs
@@ -0,0 +1,15 @@
+# capitalize/1: converte a primeira letra de uma string em maiúscula.
+# downcase/1: converte uma string em minúsculas.
+# length/1: retorna o número de caracteres em uma string.
+# replace/3: substitui todas as ocorrências de uma substring em uma string por outra substring.
+# split/2: divide uma string em substrings com base em um delimitador.
+# strip/1: remove todos os espaços em branco à esquerda e à direita de uma string.
+# upcase/1: converte uma string em maiúsculas.
+
+String.capitalize("hello") # "Hello"
+String.downcase("HELLO") # "hello"
+String.length("hello") # 5
+String.replace("hello", "l", "w") # "hewwo"
+String.split("hello world", " ") # ["hello", "world"]
+String.strip(" hello ") # "hello"
+String.upcase("hello") # "HELLO"
